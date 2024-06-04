1b62c08014f979048c1c3ed8a6dfa4fce5d5c136
aferratovvf <127087513+aferratovvf@users.noreply.github.com>
2024-06-04 14:50:00 +0200

    prova push

--- src/Components/UI/HomePage/HomePage.test.tsx
+++ src/Components/UI/HomePage/HomePage.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HomePage from './HomePage';
 
-test('renders learn react link', () => {
+test('renders learn react link a', () => {
   render(<HomePage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
